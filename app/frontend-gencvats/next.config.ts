18a84d5ca0664e566a835d2f3796b860b0beb0af
Bagas Cahya Fajar Bastian <149213682+bagascahyafb@users.noreply.github.com>
2026-08-20 18:12:39 +0700

    update full docker

--- app/frontend-gencvats/next.config.ts
+++ app/frontend-gencvats/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.110.163", "localhost", "127.0.0.1", "fungistatic-luanna-aphylly.ngrok-free.dev"],
+  allowedDevOrigins: ["192.168.110.163", "localhost", "127.0.0.1"],
   typedRoutes: true,
 };
 
